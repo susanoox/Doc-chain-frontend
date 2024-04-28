8e48f075eb107be5b4d7ef6686f4a071272f4584
MrR0b0t-23 <ashwinsenthil5118@gmail.com>
2024-04-28 15:26:16 +0530

    UI issues fixed

--- mayan/apps/appearance/static/scripts/responses.js
+++ mayan/apps/appearance/static/scripts/responses.js
@@ -17,66 +17,66 @@ function isPureNumber(str) {
 
 function getBotResponse(input) {
   var currentURL = window.location.href;
-  console.log('currentURL', currentURL, !( currentURL.includes("preview") || currentURL.includes("ocr/") || currentURL.includes("summery/") || currentURL.includes("cabinets/") ))
+  console.log('currentURL', currentURL, !(currentURL.includes("preview") || currentURL.includes("ocr/") || currentURL.includes("summery/") || currentURL.includes("cabinets/")))
   let chatbotResUrl = "";
-  if (!( currentURL.includes("preview") || currentURL.includes("ocr/") || currentURL.includes("summery/") || currentURL.includes("cabinets/") ) ) {
-      chatbotResUrl = "chatbot_res";
+  if (!(currentURL.includes("preview") || currentURL.includes("ocr/") || currentURL.includes("summery/") || currentURL.includes("cabinets/"))) {
+    chatbotResUrl = "chatbot_res";
   } else {
-      var parts = currentURL.split('/');
+    var parts = currentURL.split('/');
     // Filter the array to get only the purely numeric elements
-      var docid = parts.filter(isPureNumber)[0];
-      // var docid = parts[parts.length - 2];
-      console.log("docid : ", docid, parts)
-      chatbotResUrl = `chatbot_res_with_id/${docid}`;
+    var docid = parts.filter(isPureNumber)[0];
+    // var docid = parts[parts.length - 2];
+    console.log("docid : ", docid, parts)
+    chatbotResUrl = `chatbot_res_with_id/${docid}`;
   }
   console.log("docid : ", docid, parts, currentURL)
-    $.ajax({
-      type: "GET",
-      url: chatbotResUrl,
-      data: {
-        message: input,
-      },
-      success: function (data) {
-        let botHtml = '<p class="botText"><div style="color: white; padding: 10px; background: #04724D; width: 290px; margin-left: 10px; display: flex; border-radius: 0px 20px 20px 12px; align-items: flex-start; justify-content: flex-start; text-align: justify; flex-direction: column; font-size: 12px; font-weight: 500;">' + data.response + '</div></p>';
-        $("#chatbox").append(botHtml);
-        console.log(data.response, data)
-        document.querySelector(".powered").scrollIntoView(true);
+  $.ajax({
+    type: "GET",
+    url: chatbotResUrl,
+    data: {
+      message: input,
+    },
+    success: function (data) {
+      let botHtml = '<p class="botText"><div style="color: white; padding: 10px; background: #04724D; width: fit-content; margin-left: 10px; display: flex; border-radius: 4px; align-items: flex-start; justify-content: flex-start; text-align: justify; flex-direction: column; font-size: 12px; font-weight: 500;">' + data.response + '</div></p>';
+      $("#chatbox").append(botHtml);
+      console.log(data.response, data)
+      document.querySelector(".powered").scrollIntoView(true);
 
-        return data
-      },
-      error: function (data) {
-        console.log(data)
-        return data
-      },
-    });
+      return data
+    },
+    error: function (data) {
+      console.log(data)
+      return data
+    },
+  });
 }
 
 
 
-  let botTextElements = document.querySelectorAll('.botText');
+let botTextElements = document.querySelectorAll('.botText');
 
-  // Loop through each element and attach a click event listener
-  botTextElements.forEach(function (element) {
-    element.addEventListener('click', function () {
-      // Get the text content of the clicked p tag
-      let text = this.querySelector('span').textContent;
-      // Copy the text to clipboard
-      navigator.clipboard.writeText(text)
-        .then(function() {
-          // Alert the user that the text has been copied
-          alert("Copied to clipboard: " + text);
-        })
-        .catch(function(error) {
-          console.error('Failed to copy text: ', error);
-        });
-    });
+// Loop through each element and attach a click event listener
+botTextElements.forEach(function (element) {
+  element.addEventListener('click', function () {
+    // Get the text content of the clicked p tag
+    let text = this.querySelector('span').textContent;
+    // Copy the text to clipboard
+    navigator.clipboard.writeText(text)
+      .then(function () {
+        // Alert the user that the text has been copied
+        alert("Copied to clipboard: " + text);
+      })
+      .catch(function (error) {
+        console.error('Failed to copy text: ', error);
+      });
   });
-  
-  
-  
-  
-  
-  
+});
+
+
+
+
+
+
 // function getBotResponse(input) {
 //     //rock paper scissors
 //     if (input == "rock") {
